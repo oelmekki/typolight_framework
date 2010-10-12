9854c8bf47bcfe6c6c15c175b4e7ae1e34b7f194
Olivier El Mekki <olivier@el-mekki.com>
2010-10-12 16:04:51 +0200

    ADD methods in FwPage to move pages
    Two methods have been added :
    
    $page->insertAfter( $other_page )
    $page->insertInto( $other_page )
    
    They will respectfuly insert the page after or as child of $other_page
    ( which can be a FwPage or an id ).
    
    $page is saved at the end of each of those actions, so be sure, if it
    is a new page, to check if it has valid attributes. Also, be aware of this
    when you implement yours save callbacks.

--- system/modules/framework/FwPage.php
+++ system/modules/framework/FwPage.php
@@ -111,5 +111,120 @@ class FwPage extends EModel
 
     return $page;
   }
+
+
+  public function insertAfter( $page )
+  {
+    if ( is_numeric( $page ) )
+    {
+      $page = new FwPage( $page );
+    }
+
+
+    if ( $page->id )
+    {
+      $pid = $page->pid;
+
+      if ( ! is_numeric( $pid ) )
+      {
+        throw( new Exception( "Page " . $page->id . " has no pid" ) );
+      }
+
+      $objNextSorting = $this->Database->prepare( 'select min(sorting) as sorting from tl_page where pid = ? and sorting > ?' )
+                                       ->execute( $pid, $page->sorting );
+
+      if ( ! is_null( $objNextSorting->sorting ) )
+      {
+        $nextSorting = $objNextSorting->sorting;
+
+        if ( ( ( $page->sorting + $nextSorting ) % 2 ) != 0 or $nextSorting >= 4294967295 )
+        {
+          $count          = 1;
+          $objNewSorting  = $this->Database->prepare( 'select id, sorting from tl_page where pid = ? order by sorting' )
+                                           ->execute( $pid );
+
+          while ( $objNewSorting->next() )
+          {
+            $this->Database->prepare( 'update tl_page set sorting = ? where id = ?' )
+                           ->execute( ( $count++ * 128 ), $objNewSorting->id );
+
+            if ( $objNewSorting->id == $page->id )
+            {
+              $newSorting = ( $count++ * 128 );
+            }
+          }
+        }
+
+        else
+        {
+          $newSorting = ( ( $page->sorting + $nextSorting ) / 2 );
+        }
+      }
+
+      else
+      {
+        $newSorting = ( $page->sorting + 128 );
+      }
+
+      $this->pid      = $pid;
+      $this->sorting  = intval( $newSorting );
+
+      return $this->save();
+    }
+
+    return false;
+  }
+
+  public function insertInto( $page )
+  {
+    if ( is_numeric( $page ) )
+    {
+      $page = new FwPage( $page );
+    }
+
+    if ( $page->id )
+    {
+      $objSorting = $this->Database->prepare( "select min(sorting) as sorting from tl_page where pid = ?" )
+                                   ->execute( $page->id );
+
+      if ( $objSorting->numRows )
+      {
+        $curSorting = $objSorting->sorting;
+
+
+        // rehash sorting
+        if ( ( $curSorting % 2 ) != 0 || $curSorting < 1 )
+        {
+          $objNewSorting = $this->Database->prepare( 'select id, sorting from tl_page where pid = ? order by sorting' )
+                                          ->execute( $page->id );
+          $count      = 2;
+          $newSorting = 128;
+
+          while ( $objNewSorting->next() )
+          {
+            $this->Database->prepare( 'update tl_page set sorting = ? where id = ?' )->limit(1)
+                           ->execute( ( $count++ * 128 ), $objNewSorting->id );
+          }
+        }
+
+        else
+        {
+          $newSorting = $curSorting / 2;
+        }
+      }
+
+      else
+      {
+        $newSorting = 128;
+      }
+
+      $this->pid      = $page->id;
+      $this->sorting  = intval( $newSorting );
+
+      return $this->save();
+    }
+
+    return false;
+  }
 }
 
